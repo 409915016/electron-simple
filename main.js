2fb103fb73e43fb030a5b1150cc2f62fc0730542
Mather <409915016@qq.com>
2017-09-22 00:33:03 +0800

    + First commit.

---
+++ main.js
@@ -0,0 +1,110 @@
+const electron = require("electron");
+const url = require("url");
+const path = require("path");
+
+const { app, BrowserWindow, Menu, ipcMain } = electron;
+
+//ENV
+
+process.env.NODE_ENV = 'production';
+
+
+let mainWindow;
+let addWindow;
+
+app.on("ready", function() {
+  mainWindow = new BrowserWindow({});
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "mainWindow.html"),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+  mainWindow.on("close", function() {
+    app.quit();
+  });
+  //menu
+
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+
+  Menu.setApplicationMenu(mainMenu);
+});
+
+//createAddWindow
+function createAddWindow() {
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: "Add Shopping List Item"
+  });
+  addWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "addWindow.html"),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+
+  addWindow.on("close", function() {
+    addWindow = null;
+  });
+}
+
+//catch item:add
+
+ipcMain.on("item:add", function(e, item) {
+  mainWindow.webContents.send("item:add", item);
+  addWindow.close();
+});
+
+const mainMenuTemplate = [
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Add Item",
+        click() {
+          createAddWindow();
+        }
+      },
+      {
+        label: "Clear Item",
+        click() {
+          mainWindow.webContents.send("item:clear");
+        }
+      },
+      {
+        label: "Quit",
+        accelerator: process.platform == "darwin" ? "Command+Q" : "Ctrl+Q",
+        click() {
+          app.quit();
+        }
+      }
+    ]
+  }
+];
+
+// if mac
+if (process.platform == "darwin") {
+  mainMenuTemplate.unshift({});
+}
+
+//developer tools
+if (process.env.NODE_ENV !== "production") {
+  mainMenuTemplate.push({
+    label: "Developer Tools",
+    submenu: [
+      {
+        label: "Toggle DevTools",
+        accelerator: process.platform == "darwin" ? "Command+I" : "Ctrl+I",
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        }
+      },
+      {
+        role: "reload"
+      }
+    ]
+  });
+}
